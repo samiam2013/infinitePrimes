69a1601981e12fe1bc7ea2cc7d4726ee57225816
Samuel Myres <samiam2013@gmail.com>
2024-08-19 12:49:08 -0500

    Update gen.go

--- primes/gen.go
+++ primes/gen.go
@@ -4,23 +4,12 @@ import (
 	"math/big"
 )
 
-/*
-	func Reversed[V any](s []V) iter.Seq[V] {
-		return func(yield func(V) bool) {
-			for i := len(s) - 1; i >= 0; i-- {
-				if !yield(s[i]) {
-					return
-				}
-			}
-		}
-	}
-*/
-// func GenCandidates() iter.Seq[*big.Int] bool) {
-func GenCandidates(start *big.Int) func(yield func(v *big.Int) bool) {
-	return func(yield func(v *big.Int) bool) {
-		for i := new(big.Int).Set(start); ; i.Add(i, big.NewInt(2)) {
-			if !yield(i) {
-				return
+func GenCandidates(start *big.Int) func(yield func(v *big.Int) bool) { 
+// func GenCandidates() iter.Seq[*big.Int] bool) { // /iter.Seq(2?)/ simplifies the func(yeild func(...
+	return func(yield func(v *big.Int) bool) {  // a function that go will generate from the `for v := range ...`
+		for i := new(big.Int).Set(start); ; i.Add(i, big.NewInt(2)) { // i = start; ; i++ but with arbitrarily large integers
+			if !yield(i) { // output using the function, 
+				return // and if the for loop we're driving has exited, return
 			}
 		}
 	}
